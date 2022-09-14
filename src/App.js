365f3cac031981487ac51a618b8b193f2624509a
Bibek Mishra <deesirouss@gmail.com>
2022-09-14 23:42:53 +0545

    pipeline

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello Beautiful Peaple !
+	  How was the DevOps Part 2 session !
         </p>
         <a
           className="App-link"
